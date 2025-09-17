bbfdd8ad3828b5f9c58300046b5c04ce61f4c2ef
Hatim Lahwaouir <hlahwaou@e3r7p12.1337.ma>
2025-09-17 10:03:30 +0100

    adding alpine

--- p1/scripts/setup_agent.sh
+++ p1/scripts/setup_agent.sh
@@ -1,5 +1,4 @@
 
-sudo ufw disable
 token=$(cat /home/vagrant/conf/token)
 
 curl -sfL https://get.k3s.io | K3S_URL="https://$1:6443" INSTALL_K3S_EXEC="--node-ip=$2" K3S_TOKEN=$token sh - 
--- p1/scripts/setup_master.sh
+++ p1/scripts/setup_master.sh
@@ -1,5 +1,4 @@
 
-sudo ufw disable
 # installing master node 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$1" sh -
 
