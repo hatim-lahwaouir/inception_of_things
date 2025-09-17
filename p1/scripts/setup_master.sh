a40bd88654749dd63375f852040a7adc2e0f7037
Hatim Lahwaouir <hlahwaou@e2r5p9.1337.ma>
2025-09-17 11:16:38 +0100

    p1 is fully done using alpine

--- p1/scripts/setup_master.sh
+++ p1/scripts/setup_master.sh
@@ -2,5 +2,12 @@
 # installing master node 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$1" sh -
 
+while [ ! -f /var/lib/rancher/k3s/server/node-token ]
+do
+	echo "file doesn't exists"
+	sleep 1 
+done
+
+
 cat /var/lib/rancher/k3s/server/node-token > /home/vagrant/conf/token
 
